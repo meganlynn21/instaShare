e6c23640eb4ac23cb386401ccbd2754810900870
meganlynn21 <meganlkeyes0203@gmail.com>
2022-10-13 17:34:56 -0400

    set up delete comments

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -26,8 +26,25 @@ module.exports = {
                 return post;
             } else throw new UserInputError('Post not found');
 
+        },
+        async deleteComment(_, { postId, commentId }, context){
+            const { username } = checkAuth(context);
 
+            const post = await Post.findById(postId)
 
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else{
+                    throw new AuthenticationError('Action not found');
+                }
+            } else{
+                throw new UserInputError('Post not found');
+            }
         }
     }
-}
+};
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -55,6 +55,7 @@ module.exports = {
         } catch (err) {
           throw new Error(err);
         }
-      }
+      },
+      
     }
 };
