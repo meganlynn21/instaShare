f67a5649e7a255be8575578346060d4aa20f65f2
meganlynn21 <meganlkeyes0203@gmail.com>
2022-11-20 16:57:45 -0500

    made changes to json

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-// Dependency Imports
-const {ApolloServer} = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-// Relative imports
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const pubsub = require("graphql-subscriptions");
+
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({ req })
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
-    .then(() => {
-        console.log('MongoDB Connected');
-        return server.listen({ port: 5000});
-    })
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    });
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
