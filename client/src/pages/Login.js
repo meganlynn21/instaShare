e7512bd9ad852819051632630c36c195b0a5f873
meganlynn21 <meganlkeyes0203@gmail.com>
2022-12-14 20:30:18 -0500

    Put login and register page forms into a grid container for better layout

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -31,40 +31,46 @@ function Login(props) {
   }
 
   return (
-    <div className="form-container">
-      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
-        <h1>Login</h1>
-        <Form.Input
-          label="Username"
-          placeholder="Username.."
-          name="username"
-          type="text"
-          value={values.username}
-          error={errors.username ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Password"
-          placeholder="Password.."
-          name="password"
-          type="password"
-          value={values.password}
-          error={errors.password ? true : false}
-          onChange={onChange}
-        />
-        <Button type="submit" id="login-button" primary>
-          Login
-        </Button>
-      </Form>
-      {Object.keys(errors).length > 0 && (
-        <div className="ui error message">
-          <ul className="list">
-            {Object.values(errors).map((value) => (
-              <li key={value}>{value}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <div className="ui container grid">
+      <div className="form-container">
+        <Form
+          onSubmit={onSubmit}
+          noValidate
+          className={loading ? "loading" : ""}
+        >
+          <h1>Login</h1>
+          <Form.Input
+            label="Username"
+            placeholder="Username.."
+            name="username"
+            type="text"
+            value={values.username}
+            error={errors.username ? true : false}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Password"
+            placeholder="Password.."
+            name="password"
+            type="password"
+            value={values.password}
+            error={errors.password ? true : false}
+            onChange={onChange}
+          />
+          <Button type="submit" id="login-button" primary>
+            Login
+          </Button>
+        </Form>
+        {Object.keys(errors).length > 0 && (
+          <div className="ui error message">
+            <ul className="list">
+              {Object.values(errors).map((value) => (
+                <li key={value}>{value}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
